da32129751562c5322b4454426df2766d056c8c8
cvs <cvs>
1996-11-21 17:19:02 +0000

    Nettoyage Irene

--- thotlib/dialogue/curs.c
+++ thotlib/dialogue/curs.c
@@ -23,8 +23,6 @@
 void                InitCurs ()
 {
 #ifndef NEW_WILLOWS
-   CursCmd (0) = XCreateFontCursor (GDp (0), XC_crosshair);
-   CursSel (0) = XCreateFontCursor (GDp (0), XC_top_left_arrow);
    CursFen (0) = XCreateFontCursor (GDp (0), XC_hand2);
    CursDepV (0) = XCreateFontCursor (GDp (0), XC_sb_v_double_arrow);
    CursDepH (0) = XCreateFontCursor (GDp (0), XC_sb_h_double_arrow);
