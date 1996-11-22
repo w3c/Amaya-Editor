d54c4254ba491e7599aa17f1769ddca502ce152b
cvs <cvs>
1996-11-22 09:18:07 +0000

    *** empty log message ***

--- thotlib/internals/f/pixmapdrvr.f
+++ thotlib/internals/f/pixmapdrvr.f
@@ -2,14 +2,12 @@
 #ifndef __CEXTRACT__
 #ifdef __STDC__
 
-extern void PixmapPrintErrorMsg ( int ErrorNumber );
 extern Drawable PixmapCreateImage ( char * fn, PictureScaling pres, int *xif, int *yif, int *wif, int *hif, unsigned long BackGroundPixel, Drawable *mask1 );
 extern void PixmapPrintImage ( char * fn, PictureScaling pres, int xif, int yif, int wif, int hif, int xcf, int ycf, int wcf, int hcf, int fd, unsigned long BackGroundPixel );
 extern boolean PixmapIsFormat ( char * fn );
 
 #else /* __STDC__ */
 
-extern void PixmapPrintErrorMsg (/* int ErrorNumber */);
 extern Drawable PixmapCreateImage (/* char * fn, PictureScaling pres, int *xif, int *yif, int *wif, int *hif, unsigned long BackGroundPixel, Drawable *mask1 */);
 extern void PixmapPrintImage (/* char * fn, PictureScaling pres, int xif, int yif, int wif, int hif, int xcf, int ycf, int wcf, int hcf, int fd, unsigned long BackGroundPixel */);
 extern boolean PixmapIsFormat (/* char * fn */);
