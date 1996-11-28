7afacf62cd163af0be1521419e90533f9733ccc5
cvs <cvs>
1996-11-28 14:17:20 +0000

    Cleaned initpses.c, Daniel.

--- thotlib/view/initpses.c
+++ thotlib/view/initpses.c
@@ -1,18 +1,10 @@
-
-/* -- Copyright (c) 1990 - 1994 Inria/CNRS  All rights reserved. -- */
-
-/*=======================================================================*/
-/*|                                                                     | */
-/*|                            Projet THOT                              | */
-/*|                                                                     | */
-/*|     Module d'initialisation des e/s PostScript.                     | */
-/*|                                                                     | */
-/*|                                                                     | */
-/*|                     I. Vatton       Fevrier 92                      | */
-/*|                                                                     | */
-/*|     France Logiciel numero de depot 88-39-001-00                    | */
-/*|                                                                     | */
-/*=======================================================================*/
+/*
+ * inites.c : module handling colors and patterns in the context of
+ *	      drawing on a Postscript page (inites is for screen output).
+ *
+ * some functions are just fake ones, not used on Postcript output, but needed
+ * at link time.
+ */
 
 #include "thot_sys.h"
 #include "constmenu.h"
@@ -29,10 +21,10 @@
 #include "thotcolor_tv.h"
 
 
-/* ---------------------------------------------------------------------- */
-/* | FindOutColor finds the closest color by allocating it, or picking  | */
-/* |            an already allocated color.                             | */
-/* ---------------------------------------------------------------------- */
+/** ----------------------------------------------------------------------
+ *   FindOutColor finds the closest color by allocating it, or picking
+ *              an already allocated color.
+ *  ---------------------------------------------------------------------- **/
 #ifdef __STDC__
 void                FindOutColor (Display * dsp, Colormap colormap, ThotColorStruct * colr)
 #else  /* __STDC__ */
@@ -45,11 +37,9 @@ ThotColorStruct    *colr;
 {
 }
 
-/* ---------------------------------------------------------------------- */
-/* |    InitDocColors initialise les couleurs des documents.            | */
-/* |    La proce'dure lit le fichier des couleurs et cre'e la table     | */
-/* |    des couleurs.                                                   | */
-/* ---------------------------------------------------------------------- */
+/** ----------------------------------------------------------------------
+ *      InitDocColors initialize the Thot internal color table. (fake)
+ *  ---------------------------------------------------------------------- **/
 #ifdef __STDC__
 void                InitDocColors (char *name)
 #else  /* __STDC__ */
@@ -60,9 +50,9 @@ char               *name;
 {
 }
 
-/* ---------------------------------------------------------------------- */
-/* |    ColorPixel      retourne la valeur de la couleur de numero num. | */
-/* ---------------------------------------------------------------------- */
+/** ----------------------------------------------------------------------
+ *      ColorPixel      returns the value of a color in Thot color table.
+ *  ---------------------------------------------------------------------- **/
 #ifdef __STDC__
 unsigned long       ColorPixel (int num)
 #else  /* __STDC__ */
@@ -75,12 +65,12 @@ int                 num;
 }
 
 
-/* ---------------------------------------------------------------------- */
-/* |    ColorRGB        retourne les trois composantes rouge-vert-bleu  | */
-/* |            de la couleur de numero num.                            | */
-/* |            Si la couleur n'existe pas le proce'dure rend les       | */
-/* |            trois composantes de la couleur par de'faut.            | */
-/* ---------------------------------------------------------------------- */
+/** ----------------------------------------------------------------------
+ *      ColorRGB        returns the Red Green and Blue values corresponding
+ *		to color number num.
+ *		If the color doesn't exist the function returns the values
+ *		for the default color.
+ *  ---------------------------------------------------------------------- **/
 #ifdef __STDC__
 void                ColorRGB (int num, unsigned short *red, unsigned short *green, unsigned short *blue)
 #else  /* __STDC__ */
@@ -107,13 +97,12 @@ unsigned short     *blue;
 }
 
 
-/* ---------------------------------------------------------------------- */
-/* |    CreatePattern charge et retourne une pixmap du motif donne'     | */
-/* |            Le parame`tre active indique s'il s'agit d'une boi^te   | */
-/* |            active (1) ou non (0).                                  | */
-/* |            Les parame`tres fg, bg, motif indiquent la couleur du   | */
-/* |            trace', la couleur du fond et le motif.                 | */
-/* ---------------------------------------------------------------------- */
+/** ----------------------------------------------------------------------
+ *      CreatePattern loads and return a pixmap pattern.
+ *              active parameter indicate if the box is active.
+ *              parameters fg, bg, and motif indicate respectively
+ *              the drawing color, background color and the pattern.
+ *  ---------------------------------------------------------------------- **/
 #ifdef __STDC__
 unsigned long       CreatePattern (int disp, int RO, int active, int fg, int bg, int motif)
 #else  /* __STDC__ */
@@ -129,7 +118,7 @@ int                 motif;
 {
    unsigned long       FgPixel;
    unsigned long       BgPixel;
-   Pixmap              trame;
+   Pixmap              pattern;
 
    FgPixel = ColorPixel (fg);
    BgPixel = ColorPixel (bg);
@@ -137,126 +126,125 @@ int                 motif;
    switch (motif)
 	 {
 	    case 1:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray8_bits, gray8_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray8_bits, gray8_width,
 			     gray8_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 2:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray0_bits, gray0_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray0_bits, gray0_width,
 			     gray0_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 3:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray1_bits, gray1_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray1_bits, gray1_width,
 			     gray1_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 4:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray2_bits, gray2_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray2_bits, gray2_width,
 			     gray2_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 5:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray3_bits, gray3_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray3_bits, gray3_width,
 			     gray3_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 6:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray4_bits, gray4_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray4_bits, gray4_width,
 			     gray4_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 7:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray5_bits, gray5_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray5_bits, gray5_width,
 			     gray5_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 8:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray6_bits, gray6_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray6_bits, gray6_width,
 			     gray6_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 9:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray7_bits, gray7_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) gray7_bits, gray7_width,
 			     gray7_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 10:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) horiz1_bits, horiz1_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) horiz1_bits, horiz1_width,
 			    horiz1_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 11:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) horiz2_bits, horiz2_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) horiz2_bits, horiz2_width,
 			    horiz2_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 12:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) horiz3_bits, horiz3_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) horiz3_bits, horiz3_width,
 			    horiz3_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 13:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) vert1_bits, vert1_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) vert1_bits, vert1_width,
 			     vert1_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 14:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) vert2_bits, vert2_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) vert2_bits, vert2_width,
 			     vert2_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 15:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) vert3_bits, vert3_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) vert3_bits, vert3_width,
 			     vert3_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 16:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) left1_bits, left1_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) left1_bits, left1_width,
 			     left1_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 17:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) left2_bits, left2_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) left2_bits, left2_width,
 			     left2_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 18:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) left3_bits, left3_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) left3_bits, left3_width,
 			     left3_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 19:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) right1_bits, right1_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) right1_bits, right1_width,
 			    right1_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 20:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) right2_bits, right2_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) right2_bits, right2_width,
 			    right2_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 21:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) right3_bits, right3_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) right3_bits, right3_width,
 			    right3_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 22:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) square1_bits, square1_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) square1_bits, square1_width,
 			   square1_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 23:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) square2_bits, square2_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) square2_bits, square2_width,
 			   square2_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 24:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) square3_bits, square3_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) square3_bits, square3_width,
 			   square3_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 25:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) lozenge_bits, lozenge_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) lozenge_bits, lozenge_width,
 			   lozenge_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 26:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) brick_bits, brick_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) brick_bits, brick_width,
 			     brick_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 27:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) tile_bits, tile_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) tile_bits, tile_width,
 			      tile_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 28:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) sea_bits, sea_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) sea_bits, sea_width,
 			       sea_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    case 29:
-	       trame = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) basket_bits, basket_width,
+	       pattern = XCreatePixmapFromBitmapData (TtDisplay, TtRootWindow, (char *) basket_bits, basket_width,
 			    basket_height, FgPixel, BgPixel, TtWDepth);
 	       break;
 	    default:
-	       trame = None;
+	       pattern = None;
 	       break;
 	 }
 
-   return trame;
+   return pattern;
 }
-/*fin */
