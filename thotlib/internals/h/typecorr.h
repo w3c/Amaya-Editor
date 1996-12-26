d1ddb0e1d0c880d678413e3382fa12ad38ac407a
cvs <cvs>
1996-12-26 16:41:13 +0000

    JK: only did c comments

--- thotlib/internals/h/typecorr.h
+++ thotlib/internals/h/typecorr.h
@@ -6,40 +6,45 @@
  */
  
 /** typecorr.h **/
-#ifndef _TYPECORR_H_
-#define _TYPECORR_H_
+#ifndef _TYPECHECKER_H_
+#define _TYPECHECKER_H_
 
-#define NbLtr     120  /* nb max de lettres de l'alphabet < 256 isolatin1  */
+#define NbLtr     120  /* maximum number of letters in the alphabet  < 256 isolatin1  */
 
- typedef char    FileName[MAX_NAME_LENGTH];    /* nom de fichier */
+typedef char    FileName[MAX_NAME_LENGTH];    /* filename */
 
- typedef  char *PtrChaine;   /* pointeur sur la chaine des mots du dico */
- typedef  char *PtrCommuns;  /* pointeur sur la liste des nb lettres communes */
- typedef int *PtrMots;   /* pointeur sur la liste des pointeurs sur les mots */
+typedef  char *PtrChaine;   /* pointer to the word list */
+typedef  char *PtrCommuns;  /* pointer to the list of common letters */
+typedef int *PtrMots;   /* pointer to the list of pointers to the words */
 
- typedef struct _Dictionnaire *PtrDict;
+typedef struct _Dictionnaire *PtrDict;
 
-/* un dictionnaire */
+/* a dictionary */
 typedef struct _Dictionnaire
 {
-      PtrDict     DictSuivant;        /* pour le chainage des blocs libres*/
-      PtrDocument DictDoc;                  /* document utilisant ce dico */
-      Name         DictNom;                         /* nom du dictionnaire */
-      Buffer      DictDirectory;             /* directory du dictionnaire */
-      boolean     DictReadOnly;          /* dictionnaire en lecture seule */
-      boolean     DictCharge;                     /* dictionnaire charge' */
-      boolean     DictModifie;          /* le dictionnaire a ete modifie' */
-      Language    DictLangue;                /* la langue du dictionnaire */
-       int MAXcars;       /* max caracteres pour chaine et commun */
-       int MAXmots;       /* max entiers alloues pour pdico */
-      PtrChaine   chaine;       /* ptr sur les caracteres du dictionnaire */
-      PtrMots     pdico;                /* ptr sur la liste des pointeurs */
-                                       /*    sur les mots du dictionnaire */
-      PtrCommuns  commun;     /* ptr sur la liste des nb lettres communes */
-      int         plgdico[MAX_WORD_LEN];     /* ptr sur les mots de meme taille */
-      int         nbmots;     /* nb effectif de mots dans le dictionnaire */
-      int         nbcars;                  /* nb car effectif dans chaine */
+      PtrDict     DictSuivant;              /* for linking to free blocks */
+      PtrDocument DictDoc;                  /* document which uses this dictionary */
+      Name         DictNom;                 /* name of the dictionary */
+      Buffer      DictDirectory;            /* directory of the dictionary */
+      boolean     DictReadOnly;             /* dictionary is read only  */
+      boolean     DictCharge;               /* dictionary is loaded */
+      boolean     DictModifie;              /* dictionnary has been modified */
+      Language    DictLangue;               /* the language of the dictionary */
+       int MAXcars;       /* max characters for string and for common */
+       int MAXmots;       /* max allocated integers for pdict */
+      PtrChaine   chaine;       /* pointer to the characters of the dictionary */
+      PtrMots     pdico;                /* pointer to the list of pointers */
+                                       /*  to the words of the dictionary */
+      PtrCommuns  commun;     /* pointer to the list of common letters */
+      int         plgdico[MAX_WORD_LEN];     /* pointer to the words having the same size */
+      int         nbmots;     /*number of words in the dictionary */
+      int         nbcars;     /* number of characters in a string */
 
 }    Dictionnaire;
  
 #endif
+
+
+
+
+
