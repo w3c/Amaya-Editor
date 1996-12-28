407b1837049251794168ac9269dae6f183ae5fef
cvs <cvs>
1996-12-28 04:53:26 +0000

    JK: finished FVC comments on this module

--- thotlib/internals/h/typecorr.h
+++ thotlib/internals/h/typecorr.h
@@ -6,15 +6,16 @@
  */
  
 /** typecorr.h **/
+
 #ifndef _TYPECHECKER_H_
 #define _TYPECHECKER_H_
 
-#define NbLtr     120  /* maximum number of letters in the alphabet  < 256 isolatin1  */
+#define NbLtr     120  /* maxm number of letters in the alphabet  < 256 isolatin1  */
 
 typedef char    FileName[MAX_NAME_LENGTH];    /* filename */
 
 typedef  char *PtrChaine;   /* pointer to the word list */
-typedef  char *PtrCommuns;  /* pointer to the list of common letters */
+typedef  char *PtrCommuns;  /* pointer to the list of shared prefix length */
 typedef int *PtrMots;   /* pointer to the list of pointers to the words */
 
 typedef struct _Dictionnaire *PtrDict;
@@ -30,7 +31,7 @@ typedef struct _Dictionnaire
       boolean     DictCharge;               /* dictionary is loaded */
       boolean     DictModifie;              /* dictionnary has been modified */
       Language    DictLangue;               /* the language of the dictionary */
-       int MAXcars;       /* max characters for string and for common */
+       int MAXcars;       /* max characters for string and for prefix */
        int MAXmots;       /* max allocated integers for pdict */
       PtrChaine   chaine;       /* pointer to the characters of the dictionary */
       PtrMots     pdico;                /* pointer to the list of pointers */
@@ -38,7 +39,7 @@ typedef struct _Dictionnaire
       PtrCommuns  commun;     /* pointer to the list of common letters */
       int         plgdico[MAX_WORD_LEN];     /* pointer to the words having the same size */
       int         nbmots;     /*number of words in the dictionary */
-      int         nbcars;     /* number of characters in a string */
+      int         nbcars;     /* real number of characters in string */
 
 }    Dictionnaire;
  
@@ -47,4 +48,4 @@ typedef struct _Dictionnaire
 
 
 
-
+ 
