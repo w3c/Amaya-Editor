f4d1fbf36bd9cf204d0d84a50ba769da700feca5
cvs <cvs>
1997-01-21 12:34:57 +0000

    Ajout de deux nouvelles actions pour parametrer la determination      d'elements Hidden ou ReadOnly.
           Dominique.    21 janvier 1997.

--- thotlib/internals/h/appdialogue.h
+++ thotlib/internals/h/appdialogue.h
@@ -166,7 +166,11 @@ typedef struct _SchemaMenu_Ctl
 #define T_searchemptyelt 87
 #define T_searchemptyref 88
 #define T_searchrefto 89
-#define MAX_LOCAL_ACTIONS 90
+
+#define T_checkHiddenElement 90
+#define T_checkReadOnlyElement 91
+
+#define MAX_LOCAL_ACTIONS 92
 
 /* Indices des actions internes accessibles au clavier et
    non presentees dans les menus */
