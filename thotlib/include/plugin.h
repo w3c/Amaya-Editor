65fba758e29e3a2e10111c665aab48c016c7c089
cvs <cvs>
1997-01-30 10:10:57 +0000

    Irene

---
+++ thotlib/include/plugin.h
@@ -0,0 +1,23 @@
+/*
+ *
+ *  (c) COPYRIGHT MIT and INRIA, 1996.
+ *  Please first read the full copyright statement in file COPYRIGHT.
+ *
+ */
+
+#ifndef _PLUGIN_H_
+#define _PLUGIN_H_
+
+#ifndef __CEXTRACT__
+#ifdef __STDC__
+extern void TtaCreateFormPlugin (Document document,
+			      View view);
+
+#else  /* __STDC__ */
+
+#endif /* __STDC__ */
+#endif /* __CEXTRACT__ */
+extern void TtaCreateFormPlugin (/* Document document,
+				 View view */);
+
+#endif
