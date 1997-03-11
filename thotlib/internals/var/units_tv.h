4cc9b0fadbd79fbb2a056504180680f64032d7df
cvs <cvs>
1997-03-11 15:48:34 +0000

    Patches for compiling with ThotKernel. Staphane.

---
+++ thotlib/internals/var/units_tv.h
@@ -0,0 +1,25 @@
+/*
+ *
+ *  (c) COPYRIGHT INRIA, 1996.
+ *  Please first read the full copyright statement in file COPYRIGHT.
+ *
+ */
+
+/*
+ * Warning:
+ * This module is part of the Thot library, which was originally
+ * developed in French. That's why some comments are still in
+ * French, but their translation is in progress and the full module
+ * will be available in English in the next release.
+ *
+ */
+
+/*
+ * Global variables for units management
+ */
+
+#ifndef THOT_UNITS_VAR
+#define THOT_UNITS_VAR
+
+THOT_EXPORT int      DOT_PER_INCHE;  /* screen resolution */
+#endif
