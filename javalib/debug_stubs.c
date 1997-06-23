e3ff1b4406a146c27c0ee0036cf2161695881928
cvs <cvs>
1997-06-23 15:37:43 +0000

    New files for debug purposes, Daniel.

---
+++ javalib/debug_stubs.c
@@ -0,0 +1,42 @@
+/*
+ * Kaffe stubs generated for class thotlib_Extra.
+ * This class encapsulate call to the Thot lib api which could not
+ * be built automatically by javastub program.
+ */
+
+#include <native.h>
+#include "app.h"
+#include "appaction.h"
+#include "memory.h"
+#include "JavaTypes.h"
+#include "thotlib_Action.h"
+#include "debug_stubs.h"
+
+#include "JavaTypes_f.h"
+
+void
+thotlib_Element_Debug(struct Hthotlib_Element* el)
+{
+}
+
+/*
+ * Java to C function Ttaxxx stub.
+xxx
+thotlib_Extra_Ttaxxx(struct Hthotlib_Extra* none, xxx)
+{
+}
+ */
+
+/*
+ * Function to register all thotlib_Selection stubs.
+ */
+void register_debug_stubs(void)
+{
+	addNativeMethod("thotlib_Element_Debug",
+	                thotlib_Element_Debug);
+
+/*
+	addNativeMethod("thotlib_Extra_Ttaxxx", thotlib_Extra_Ttaxxx);
+ */
+}
+
---
+++ javalib/debug_stubs.h
@@ -0,0 +1,12 @@
+/*
+ * Debugging stubs.
+ */
+
+#ifndef __DEBUG_STUBS_H__
+#define __DEBUG_STUBS_H__
+
+#include "JavaTypes.h"
+
+void thotlib_Element_Debug(struct Hthotlib_Element* el);
+#endif /* __DEBUG_STUBS_H__ */
+
