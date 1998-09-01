9ae7ceb30caad220a2de33698c0bd238dc6a42c5
cvs <cvs>
1998-09-01 10:58:57 +0000

    Ramzi.

--- thotlib/internals/f/interface_f.h
+++ thotlib/internals/f/interface_f.h
@@ -5,15 +5,18 @@
 
 #ifndef __CEXTRACT__
 #ifdef __STDC__
-
+#ifndef _WINDOWS
 extern int TtaXLookupString ( ThotKeyEvent *event,
                               char *buffer,
                               int nbytes,
                               KeySym *keysym,
                               ThotComposeStatus *status );
+#endif /* _WINDOWS */
 extern void TtaInstallMultiKey ( void );
+#ifndef _WINDOWS
 extern int TtaGetIsoKeysym ( ThotEvent * event,
                              KeySym keysym );
+#endif /* _WINDOWS */
 extern int TtaHandleMultiKeyEvent ( ThotEvent * event );
 extern void TtaSetMainLoop ( ExternalInitMainLoop init,
                              ExternalMainLoop loop,
