91820f9b5238b875974db66599e1174e7368e163
cvs <cvs>
1999-02-19 14:29:24 +0000

    Installation provisoire de exc_Table.h pour la compilation d'Amaya, Daniel.

---
+++ tablelib/exc_Table.h
@@ -0,0 +1,76 @@
+
+#define EXC_TR_Table_STRUCTURE	508
+#define EXC_TR_Table_CREATION		510
+#define EXC_TR_Table_PASTE		513
+#define EXC_TR_Table_SELECT		512
+#define EXC_TR_Table_ATTRIBUT		514
+#define EXC_ID_Table			509
+
+#define EXC_ID_Title_Table		559
+
+#define EXC_ID_Headings			560
+
+#define EXC_ID_The_Columns		561
+
+#define EXC_ID_The_Raws		562
+
+#define EXC_ID_Column			563
+
+#define EXC_ID_Simple_Column		564
+#define EXC_TR_Simple_Column_SUPPRES	511
+			
+#define EXC_ID_Title_Simple_Col 	577
+
+#define EXC_ID_Compound_Column		565
+#define EXC_TR_Compound_Column_SUPPRES	511
+			
+#define EXC_ID_Title_Compound_Col	566
+
+#define EXC_ID_Sub_Columns		567
+
+#define EXC_ID_Raw			568
+
+#define EXC_ID_Simple_Raw		569
+			
+#define EXC_ID_Title_Raw		570
+
+#define EXC_ID_Body_Raw		571
+
+#define EXC_ID_Compound_Raw 		572
+			
+#define EXC_ID_Title_Compound_Raw		573
+
+#define EXC_ID_Sub_Raws		574
+
+#define EXC_ID_FootTable		578
+
+#define EXC_ID_Cell			575
+#define EXC_TR_Cell_ATTRIBUT		576
+
+
+
+
+#define EXC_ID_Left_Hairline_Cols	521
+#define EXC_ID_Right_Hairline_SimpCol	522
+#define EXC_ID_Foot_Hairline_CompCol	523
+#define EXC_ID_Foot_Hairline_Headings	526
+#define EXC_ID_Foot_Hairline_SimpRaw	528
+#define EXC_ID_Right_Hairline_CompRaw	529
+
+#define EXC_ID_Title_Width	 	540
+#define EXC_ID_Ref_Title	 	541
+#define EXC_ID_Width_Percent	 	542
+
+#define EXC_ID_Column_Width	 	543
+#define EXC_ID_Ref_Column	 	544
+
+#define EXC_ID_Type_Table		545
+#define EXC_ID_Table_Frame		550
+
+#define EXC_ID_Align_Horiz		546
+#define EXC_ID_Align_HorizDistrib	547
+#define EXC_ID_Align_Vertic		548
+#define EXC_ID_Align_VerticDistrib	549
+
+#define EXC_ID_Extens_Horiz		551
+#define EXC_ID_Extens_Vertic		552
