b51611fea607475d50aa7fbb05c4a13f803f3d5a
cvs <cvs>
1999-08-23 16:23:58 +0000

    Changed the value of the maximum string length

--- thotlib/include/profiles.h
+++ thotlib/include/profiles.h
@@ -11,11 +11,11 @@
 
 
 #define THOT_EXPORT extern
-#define MAX_FUNCTIONS              1000    /* Maximum of functions defined in Amaya software */       
-#define MAX_DEF                    1000      /* Maximun of lines for the module definition file */
-#define MAX_EDITION_FUNCTIONS       500  /* Maximum of functions that are used for edition */
-#define MAX_PRO                      50         /* Maximun of profile displayed in the interface */
-#define MAX_PRO_LENGTH               50       /* Maximum length of a string */
+#define MAX_FUNCTIONS              2000    /* Maximum of functions defined in Amaya software */       
+#define MAX_DEF                    3000      /* Maximun of lines for the module definition file */
+#define MAX_EDITION_FUNCTIONS       100  /* Maximum of functions that are used for edition */
+#define MAX_PRO                      50         /* Maximum of profile displayed in the menu */
+#define MAX_PRO_LENGTH              200       /* Maximum length of a string */
 #define DEF_FILE                   "ProfileDefs"
 #define PROFILE_START              '['
 #define MODULE_START               '['
