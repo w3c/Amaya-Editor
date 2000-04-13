a0d5a09ea6875920682490e6c91c3caf6644510a
kahan <kahan>
2000-04-13 14:40:30 +0000

    JK: a missing file

---
+++ annotlib/ANNOTmenu.h
@@ -0,0 +1,31 @@
+/*
+ *
+ *  (c) COPYRIGHT MIT and INRIA, 2000.
+ *  Please first read the full copyright statement in file COPYRIGHT.
+ *
+ */
+
+#ifndef _ANNOTQUERY_H__
+#define _ANNOTQUERY_H__
+/*----------------------------------------------------------------------
+     Definitions of the menu entries used in the Query Customization
+     menu (ANNOTquery.c)
+     Jose Kahan April 2000
+  ----------------------------------------------------------------------*/
+
+/* dialog and messages */
+
+#define CustomQueryMenu        1
+#define mUsersGroups           2
+#define mUser1                 3
+#define mUser2                 4
+#define mTime                  5
+#define mBtime                 6
+#define mEtime                 7
+#define mExpertMode            8
+#define mFreeText              9
+#define MAX_QUERYCONFMENU_DLG  10
+
+#endif /*_ANNOTquery.h */
+
+
