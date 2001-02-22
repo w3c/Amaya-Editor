f0aee0f3ce4665ef5cf61b22de8dfdb5eff1e645
cvs <cvs>
2001-02-22 15:53:22 +0000

    The libpng generated a warning. Irene

--- thotlib/image/pnghandler.c
+++ thotlib/image/pnghandler.c
@@ -1,17 +1,8 @@
 /*
  *
- *  (c) COPYRIGHT INRIA, 1996.
+ *  (c) COPYRIGHT INRIA, 1996-2001.
  *  Please first read the full copyright statement in file COPYRIGHT.
  *
- */
-
-/*
- * Warning:
- * This module is part of the Thot library, which was originally
- * developed in French. That's why some comments are still in
- * French, but their translation is in progress and the full module
- * will be available in English in the next release.
- * 
  */
 
 /*
@@ -141,325 +132,28 @@ char* typecouleur[] = {"grayscale", "undefined type", "RGB",
 extern BOOL pic2print;
 #endif /* _WINDOWS */
 
-#ifdef __STDC__
-static void my_png_error (png_struct* png_ptr, STRING message)
-#else  /* !__STDC__ */
-static void my_png_error (png_ptr, message)
-png_struct* png_ptr; 
-STRING       message;
-#endif /* __STDC__ */
+/*----------------------------------------------------------------------
+  ----------------------------------------------------------------------*/
+static void my_png_error (png_struct *png_ptr, STRING message)
 {
    fprintf(stderr,"libpng error: %s\n", message);
    longjmp(png_ptr->jmpbuf, 1);
 }
 
-#ifdef __STDC__
-static void my_png_warning (png_struct* png_ptr, STRING message)
-#else  /* !__STDC__ */
-static void my_png_warning (png_ptr, message)
-png_struct* png_ptr; 
-STRING       message;
-#endif /* __STDC__ */
+/*----------------------------------------------------------------------
+  ----------------------------------------------------------------------*/
+static void my_png_warning (png_struct *png_ptr, STRING message)
 {
    if (!png_ptr)
      return;
    fprintf(stderr,"libpng warning: %s\n", message);
 }
  
-#ifdef __STDC__
-unsigned char *ReadPng (FILE* infile, int* width, int* height, int* ncolors, int* cpp, ThotColorStruct colrs[256], int* bg)
-
-#else /* __STDC__ */
-unsigned char *ReadPng (infile, width, height, ncolors, cpp, colrs, bg)
-FILE *infile;
-int *width; 
-int *height; 
-int *ncolors; 
-int *cpp; 
-ThotColorStruct colrs[256];
-int *bg;
-#endif /* __STDC__ */
+/*----------------------------------------------------------------------
+  ----------------------------------------------------------------------*/
+unsigned char *ReadPng (FILE *infile, int *width, int *height, int *ncolors,
+			int *cpp, ThotColorStruct colrs[256], int *bg)
 {
-#ifdef IV
-  png_struct     png_ptr;
-  png_info       info_ptr;
-  png_uint_32 width, height;
-  int bit_depth, color_type, interlace_type;
-
-   /* Create and initialize the png_struct with the desired error handler
-    * functions.  If you want to use the default stderr and longjump method,
-    * you can supply NULL for the last three parameters.  We also supply the
-    * the compiler header file version, so that we know if the application
-    * was compiled with a compatible version of the library.  REQUIRED
-    */
-   png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,
-      (void *)user_error_ptr, my_png_error, my_png_warning);
-
-   if (png_ptr == NULL)
-   {
-      fclose(infile);
-      return (NULL);
-   }
-
-   /* Allocate/initialize the memory for image information.  REQUIRED. */
-   info_ptr = png_create_info_struct(png_ptr);
-   if (info_ptr == NULL)
-   {
-      fclose(infile);
-      png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
-      return (NULL);
-   }
-
-   /* Set error handling if you are using the setjmp/longjmp method (this is
-    * the normal method of doing things with libpng).  REQUIRED unless you
-    * set up your own error handlers in the png_create_read_struct() earlier.
-    */
-   if (setjmp(png_ptr->jmpbuf))
-   {
-      /* Free all of the memory associated with the png_ptr and info_ptr */
-      png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
-      fclose(infile);
-      /* If we get here, we had a problem reading the file */
-      return (NULL);
-   }
-
-   /* One of the following I/O initialization methods is REQUIRED */
-#ifdef streams /* PNG file I/O method 1 */
-   /* Set up the input control if you are using standard C streams */
-   png_init_io(png_ptr, infile);
-
-#else /* no_streams */ /* PNG file I/O method 2 */
-   /* If you are using replacement read functions, instead of calling
-    * png_init_io() here you would call:
-    */
-   png_set_read_fn(png_ptr, (void *)user_io_ptr, user_read_fn);
-   /* where user_io_ptr is a structure you want available to the callbacks */
-#endif /* no_streams */ /* Use only one I/O method! */
-
-   /* If we have already read some of the signature */
-   png_set_sig_bytes(png_ptr, sig_read);
-
-   /* The call to png_read_info() gives us all of the information from the
-    * PNG file before the first IDAT (image data chunk).  REQUIRED
-    */
-   png_read_info(png_ptr, info_ptr);
-
-   png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type,
-       &interlace_type, NULL, NULL);
-
-/**** Set up the data transformations you want.  Note that these are all
- **** optional.  Only call them if you want/need them.  Many of the
- **** transformations only work on specific types of images, and many
- **** are mutually exclusive.
- ****/
-
-   /* tell libpng to strip 16 bit/color files down to 8 bits/color */
-   png_set_strip_16(png_ptr);
-
-   /* Strip alpha bytes from the input data without combining with th
-    * background (not recommended).
-    */
-   png_set_strip_alpha(png_ptr);
-
-   /* Extract multiple pixels with bit depths of 1, 2, and 4 from a single
-    * byte into separate bytes (useful for paletted and grayscale images).
-    */
-   png_set_packing(png_ptr);
-
-   /* Change the order of packed pixels to least significant bit first
-    * (not useful if you are using png_set_packing). */
-   png_set_packswap(png_ptr);
-
-   /* Expand paletted colors into true RGB triplets */
-   if (color_type == PNG_COLOR_TYPE_PALETTE)
-      png_set_expand(png_ptr);
-
-   /* Expand grayscale images to the full 8 bits from 1, 2, or 4 bits/pixel */
-   if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
-      png_set_expand(png_ptr);
-
-   /* Expand paletted or RGB images with transparency to full alpha channels
-    * so the data will be available as RGBA quartets.
-    */
-   if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
-      png_set_expand(png_ptr);
-
-   /* Set the background color to draw transparent and alpha images over.
-    * It is possible to set the red, green, and blue components directly
-    * for paletted images instead of supplying a palette index.  Note that
-    * even if the PNG file supplies a background, you are not required to
-    * use it - you should use the (solid) application background if it has one.
-    */
-
-   png_color_16 my_background, *image_background;
-
-   if (png_get_bKGD(png_ptr, info_ptr, &image_background))
-      png_set_background(png_ptr, image_background,
-                         PNG_BACKGROUND_GAMMA_FILE, 1, 1.0);
-   else
-      png_set_background(png_ptr, &my_background,
-                         PNG_BACKGROUND_GAMMA_SCREEN, 0, 1.0);
-
-   /* Some suggestions as to how to get a screen gamma value */
-
-   /* Note that screen gamma is (display_gamma/viewing_gamma) */
-   if (/* We have a user-defined screen gamma value */)
-   {
-      screen_gamma = user-defined screen_gamma;
-   }
-   /* This is one way that applications share the same screen gamma value */
-   else if ((gamma_str = getenv("SCREEN_GAMMA")) != NULL)
-   {
-      screen_gamma = atof(gamma_str);
-   }
-   /* If we don't have another value */
-   else
-   {
-      screen_gamma = 2.2;  /* A good guess for a PC monitors in a dimly
-                              lit room */
-      screen_gamma = 1.7 or 1.0;  /* A good guess for Mac systems */
-   }
-
-   /* Tell libpng to handle the gamma conversion for you.  The second call
-    * is a good guess for PC generated images, but it should be configurable
-    * by the user at run time by the user.  It is strongly suggested that
-    * your application support gamma correction.
-    */
-
-   int intent;
-
-   if (png_get_sRGB(png_ptr, info_ptr, &intent))
-      png_set_sRGB(png_ptr, intent, 0);
-   else 
-      if (png_get_gAMA(png_ptr, info_ptr, &image_gamma))
-         png_set_gamma(png_ptr, screen_gamma, image_gamma);
-      else
-         png_set_gamma(png_ptr, screen_gamma, 0.50);
-
-   /* Dither RGB files down to 8 bit palette or reduce palettes
-    * to the number of colors available on your screen.
-    */
-   if (color_type & PNG_COLOR_MASK_COLOR)
-   {
-      png_uint_32 num_palette;
-      png_colorp palette;
-
-      /* This reduces the image to the application supplied palette */
-      if (/* we have our own palette */)
-      {
-         /* An array of colors to which the image should be dithered */
-         png_color Std_color_cube[MAX_SCREEN_COLORS];
-
-         png_set_dither(png_ptr, Std_color_cube, MAX_SCREEN_COLORS,
-            MAX_SCREEN_COLORS, NULL, 0);
-      }
-      /* This reduces the image to the palette supplied in the file */
-      else if (png_get_PLTE(png_ptr, info_ptr, &palette, &num_palette))
-      {
-         png_color16p histogram;
-
-         png_get_hIST(png_ptr, info_ptr, &histogram);
-
-         png_set_dither(png_ptr, palette, num_palette,
-                        max_screen_colors, histogram, 0);
-      }
-   }
-
-   /* invert monocrome files to have 0 as white and 1 as black */
-   png_set_invert_mono(png_ptr);
-
-   /* If you want to shift the pixel values from the range [0,255] or
-    * [0,65535] to the original [0,7] or [0,31], or whatever range the
-    * colors were originally in:
-    */
-   if (png_get_valid(png_ptr, info_ptr, PNG_INFO_sBIT))
-   {
-      png_color8p sig_bit;
-
-      png_get_sBIT(png_ptr, info_ptr, &sig_bit);
-      png_set_shift(png_ptr, sig_bit);
-   }
-
-   /* flip the RGB pixels to BGR (or RGBA to BGRA) */
-   png_set_bgr(png_ptr);
-
-   /* swap the RGBA or GA data to ARGB or AG (or BGRA to ABGR) */
-   png_set_swap_alpha(png_ptr);
-
-   /* swap bytes of 16 bit files to least significant byte first */
-   png_set_swap(png_ptr);
-
-   /* Add filler (or alpha) byte (before/after each RGB triplet) */
-   png_set_filler(png_ptr, 0xff, PNG_FILLER_AFTER);
-
-   /* Turn on interlace handling.  REQUIRED if you are not using
-    * png_read_image().  To see how to handle interlacing passes,
-    * see the png_read_row() method below:
-    */
-   number_passes = png_set_interlace_handling(png_ptr);
-
-   /* Optional call to gamma correct and add the background to the palette
-    * and update info structure.  REQUIRED if you are expecting libpng to
-    * update the palette for you (ie you selected such a transform above).
-    */
-   png_read_update_info(png_ptr, info_ptr);
-
-   /* Allocate the memory to hold the image using the fields of info_ptr. */
-
-   /* The easiest way to read the image: */
-   png_bytep row_pointers[height];
-
-   for (row = 0; row < height; row++)
-   {
-      row_pointers[row] = malloc(png_get_rowbytes(png_ptr, info_ptr));
-   }
-
-   /* Now it's time to read the image.  One of these methods is REQUIRED */
-#ifdef entire /* Read the entire image in one go */
-   png_read_image(png_ptr, row_pointers);
-
-#else /* no_entire */ /* Read the image one or more scanlines at a time */
-   /* The other way to read images - deal with interlacing: */
-
-   for (pass = 0; pass < number_passes; pass++)
-   {
-#ifdef single /* Read the image a single row at a time */
-      for (y = 0; y < height; y++)
-      {
-         png_bytep row_pointers = row[y];
-         png_read_rows(png_ptr, &row_pointers, NULL, 1);
-      }
-
-#else /* no_single */  /* Read the image several rows at a time */
-      for (y = 0; y < height; y += number_of_rows)
-      {
-#ifdef sparkle /* Read the image using the "sparkle" effect. */
-         png_read_rows(png_ptr, row_pointers, NULL, number_of_rows);
-        
-#else /* no_sparkle */ /* Read the image using the "rectangle" effect */
-         png_read_rows(png_ptr, NULL, row_pointers, number_of_rows);
-#endif /* no_sparkle */ /* use only one of these two methods */
-      }
-     
-      /* if you want to display the image after every pass, do
-         so here */
-#endif /* no_single */ /* use only one of these two methods */
-   }
-#endif /* no_entire */ /* use only one of these two methods */
-
-   /* read rest of file, and get additional chunks in info_ptr - REQUIRED */
-   png_read_end(png_ptr, info_ptr);
-
-   /* clean up after the read, and free any memory allocated - REQUIRED */
-   png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
-
-   /* close the file */
-   fclose(infile);
-
-   /* that's it */
-   return;
-
-#else /* IV */
   png_byte      *pp;
   png_byte       buf[8];
   png_byte      *dp;
@@ -476,6 +170,7 @@ int *bg;
   int            i, j, passes;
   int            xpos, ypos;
   int            xr, xg, xb;
+  int            bit_depth, interlace_type;
 
   ret = fread (buf, 1, 8, infile);
   if (ret != 8)
@@ -486,86 +181,71 @@ int *bg;
     return(NULL);
   rewind (infile);
 
-   png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,
-		(png_voidp)my_png_error, (png_voidp)my_png_error, (png_voidp)my_png_warning);
+  png_ptr = png_create_read_struct (PNG_LIBPNG_VER_STRING, (png_voidp)my_png_error,
+				    (png_voidp)my_png_error, (png_voidp)my_png_warning);
+  if (png_ptr == NULL)
+    return NULL;
 
-    
-    if (!png_ptr)
+  /* Allocate/initialize the memory for image information.  REQUIRED. */
+  info_ptr = png_create_info_struct(png_ptr);
+  if (info_ptr == NULL)
+    {
+      png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
       return NULL;
+    }
 
-    /* png_set_error_fn(png_ptr, NULL, (png_voidp)my_png_error, (png_voidp)my_png_warning); */
-    
-    info_ptr = png_create_info_struct(png_ptr);
-    if (!info_ptr)
-      {
-	TtaFreeMemory (png_ptr);
-	return NULL;
-      }
+  if (setjmp (png_ptr->jmpbuf))
+    {
+      /* Free all of the memory associated with the png_ptr and info_ptr */
+      png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
+      /* sends a message to the user throught thot GUI */ 	
+      fprintf(stderr, "ping read image error \n");
+      if (row_pointers)
+	TtaFreeMemory (row_pointers);
+      if (pixels)
+	TtaFreeMemory (pixels);
+      if (png_pixels)
+	TtaFreeMemory (png_pixels);
+      return NULL;
+    }
 
-    if (setjmp (png_ptr->jmpbuf))
-      {
-	/* sends a message to the user throught thot GUI */ 	
-	fprintf(stderr, "ping read image error \n");
-        if (png_ptr != NULL)
-	  png_read_destroy (png_ptr, info_ptr, (png_info*)0);
-	if (png_ptr != NULL)
-	  TtaFreeMemory (png_ptr);
-	if (info_ptr != NULL)
-	  TtaFreeMemory (info_ptr);
-	if (row_pointers != NULL)
-	  TtaFreeMemory (row_pointers);
-	if (pixels != NULL)
-	  TtaFreeMemory (pixels);
-	if (png_pixels != NULL)
-	  TtaFreeMemory (png_pixels);
-	return NULL;
-      }
+   /* Set up the input control if you are using standard C streams */
+  png_init_io (png_ptr, infile);
+    
+  /* If we have already read some of the signature */
+  /*png_set_sig_bytes(png_ptr, sig_read);*/
+  /* read the file information */
+  png_read_info (png_ptr, info_ptr);
+    
+  png_get_IHDR(png_ptr, info_ptr, (png_uint_32 *)width, (png_uint_32 *)height, &bit_depth, &color_type,
+	       &interlace_type, NULL, NULL);
 
-    /* initialize the structures, info first for error handling */
-    /* png_info_init (info_ptr);
-       png_read_init (png_ptr);  */ 
-    /* png_read_png(png_ptr, info_ptr, png_transforms, NULL);*/
-    /* set up the input control for C streams*/
-    png_init_io (png_ptr, infile);
+  /* expand color components to 8 bit rgb */
+  png_set_expand (png_ptr);
     
-    /* png_read_png(png_ptr, info_ptr, png_transforms, NULL); */
-    /* read the file information */
-    png_read_info (png_ptr, info_ptr);
+  /* tell libpng to handle the gamma conversion for you */
+  if (info_ptr->valid & PNG_INFO_gAMA) 
+    gamma_correction = info_ptr->gamma;
+  else 
+    gamma_correction = (double)0.45;
+
+  if (fabs ((2.2 * gamma_correction) - 1.0) > 0.011)
+    png_set_gamma (png_ptr, (double) 2.2, gamma_correction);
     
-    /* 
-       printf(" Picture information  %ld x %ld, %d-bit, %s, %s\n",
-       info_ptr->width, info_ptr->height,
-       info_ptr->bit_depth, 
-       (info_ptr->color_type>6) ? typecouleur[1] : typecouleur[info_ptr->color_type],
-       info_ptr->interlace_type ? "entrelace" : "non-entrelace");
-       */
-    
-    /* expand color components to 8 bit rgb */
-    png_set_expand (png_ptr);
-    
-    /* tell libpng to handle the gamma conversion for you */
-    if (info_ptr->valid & PNG_INFO_gAMA) 
-      gamma_correction = info_ptr->gamma;
+  /*
+    if (info_ptr->valid & PNG_INFO_bKGD)
+    png_set_background (png_ptr, &(info_ptr->background), PNG_BACKGROUND_GAMMA_FILE, 1, gamma_correction);
     else 
-      gamma_correction = (double)0.45;
-
-    if (fabs ((2.2 * gamma_correction) - 1.0) > 0.011)
-      png_set_gamma (png_ptr, (double) 2.2, gamma_correction);
-    
-    /*
-      if (info_ptr->valid & PNG_INFO_bKGD)
-      png_set_background (png_ptr, &(info_ptr->background), PNG_BACKGROUND_GAMMA_FILE, 1, gamma_correction);
-     else 
-     {   
-     color_background.index = 0;
-     color_background.red = 65535 >> (16 - info_ptr->bit_depth);
-     color_background.green = 65535 >> (16 - info_ptr->bit_depth);
-     color_background.blue = 65535 >> (16 - info_ptr->bit_depth);
-     color_background.gray = 65535 >> (16 - info_ptr->bit_depth);
-     png_set_background(png_ptr, &color_background, 
-     PNG_BACKGROUND_GAMMA_SCREEN, 0, 1.0);
-     }
-     */
+    {   
+    color_background.index = 0;
+    color_background.red = 65535 >> (16 - info_ptr->bit_depth);
+    color_background.green = 65535 >> (16 - info_ptr->bit_depth);
+    color_background.blue = 65535 >> (16 - info_ptr->bit_depth);
+    color_background.gray = 65535 >> (16 - info_ptr->bit_depth);
+    png_set_background(png_ptr, &color_background, 
+    PNG_BACKGROUND_GAMMA_SCREEN, 0, 1.0);
+    }
+  */
     
     /* strip 16-bit channels down to 8 */ 
     if (info_ptr->bit_depth == 16)
@@ -683,7 +363,7 @@ int *bg;
 	png_error (png_ptr, "Unknown PNG color type ");
       }
 
-    png_read_update_info (png_ptr, info_ptr);
+    /*png_read_update_info (png_ptr, info_ptr);*/
     png_start_read_image (png_ptr);
     for (i = 0; i < passes; i++)
       for (j = 0; j < *height; j++)
@@ -829,7 +509,6 @@ int *bg;
     TtaFreeMemory ( png_ptr);
     TtaFreeMemory ( info_ptr);
     return (pixels);
-#endif /* IV */
 }
 
 /*----------------------------------------------------------------------
