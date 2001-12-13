8a26bdd24fc633b3176c16b425549b2dc003eabd
cvs <cvs>
2001-12-13 15:59:01 +0000

    Define a MathML profile :   MathML elements/attributes are allowed in all profiles except XHTML 1.0 Basic   A MathML document accepts only MathML elements/attributes Laurent

--- thotlib/include/profiles.h
+++ thotlib/include/profiles.h
@@ -11,19 +11,21 @@
 #define MAX_PRO_LENGTH              200       /* Maximum length of a string */
 #define DEF_FILE                   "amaya.profiles"
 
-/* Masks for the XHTML profiles */
-#define L_Other            0x00
-#define L_Basic            0x02
-#define L_Strict           0x06
-#define L_Xhtml11          0x1E
-#define L_Transitional     0x2E
+/* Masks for the profiles */
+#define L_Other             0x00 /* No one profile is defined */
+#define L_Basic             0x02 /* accept only Basic values */
+#define L_Strict            0x06 /* accept Strict and Basic values */
+#define L_Xhtml11           0x1E /* accept Strict, Basic and Ruby values */
+#define L_Transitional      0x2E /* accept all values except Ruby */
+#define L_MathML            0x40 /* accept only MathML values */
 
-/* Value for the XHTML profiles */
-#define L_BasicValue        0x02
-#define L_StrictValue       0x0C
-#define L_Xhmli11Value      0x0C
-#define L_RubyValue         0x10
-#define L_TransitionalValue 0x20
+/* Values for the profiles */
+#define L_BasicValue        0x02 /* allowed for all profiles */
+#define L_StrictValue       0x0C /* allowed for Basic and Strict profiles */
+#define L_Xhmli11Value      0x0C /* allowed for Basic, Strict and Xhmli11 profiles */
+#define L_RubyValue         0x10 /* allowed only for Xhmli11 profile */
+#define L_TransitionalValue 0x20 /* allowed only for Transitional profile */
+#define L_MathMLValue       0x7C /* allowed for all profiles except Basic */
 #define L_OtherValue        0xFF
 
 /*----------------------------------------------------------------------
