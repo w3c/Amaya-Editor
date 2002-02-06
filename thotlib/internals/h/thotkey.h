6df8caf5cb6b5c0e321bff342916ae180f2fd529
vatton <vatton>
2002-02-06 16:43:48 +0000

    The unicode version now uses right code for special spaces. Irene

--- thotlib/internals/h/thotkey.h
+++ thotlib/internals/h/thotkey.h
@@ -1,6 +1,6 @@
 /*
  *
- *  (c) COPYRIGHT INRIA, 1996.
+ *  (c) COPYRIGHT INRIA, 1996-2002
  *  Please first read the full copyright statement in file COPYRIGHT.
  *
  */
@@ -8,15 +8,24 @@
 #ifndef _THOTKEY_H_
 #define _THOTKEY_H_
 
-#define BREAK_LINE 138
-#define SHOWN_BREAK_LINE 182 /* BREAK_LINE display value */
 #define NEW_LINE 10
-#define THIN_SPACE 129
 #define SHOWN_THIN_SPACE 96 /* THIN-SPACE display value */
-#define HALF_EM 130
+#define BREAK_LINE 138
+#define UNBREAKABLE_SPACE 0xA0
+
+#ifdef _I18N_
+#define SHOWN_BREAK_LINE 0x204B /* BREAK_LINE display value */
+#define THIN_SPACE 0x2009
+#define HALF_EM 0x2005
 #define SHOWN_HALF_EM 166 /* HALF-EM display value */
-#define UNBREAKABLE_SPACE 160
 #define SHOWN_UNBREAKABLE_SPACE 185 /* UNBREAKABLE-SPACE display value */
 #define SHOWN_SPACE 183 /* SPACE display value */
-
+#else /* _I18N_ */
+#define SHOWN_BREAK_LINE 182 /* BREAK_LINE display value */
+#define THIN_SPACE 129
+#define HALF_EM 130
+#define SHOWN_HALF_EM 166 /* HALF-EM display value */
+#define SHOWN_UNBREAKABLE_SPACE 185 /* UNBREAKABLE-SPACE display value */
+#define SHOWN_SPACE 183 /* SPACE display value */
+#endif /* _I18N_ */
 #endif
