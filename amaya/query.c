fc73572062149672d54432b60cfbb76b3378aea9
kahan <kahan>
2002-04-19 09:21:26 +0000

    JK: add more XML default types to the requests. Give a preference to an application chosen  MIME type.

--- amaya/query.c
+++ amaya/query.c
@@ -2838,25 +2838,29 @@ int GetObjectWWW (int docid, char *urlName, char *formdata,
 	 else
 #endif /* ANNOTATIONS */
 	   me->method = METHOD_GET;
-       if (!HasKnownFileSuffix (ref))
+#ifdef ANNOTATIONS
+       /* use the custom sent content_type */
+       if (content_type && content_type[0] != EOS)
+	 AHTRequest_setCustomAcceptHeader (me->request, content_type);
+       else
+#endif /* ANNOTATIONS */
+	 if (!HasKnownFileSuffix (ref))
 	 {
 	   /* try to adjust the Accept header in an netwise economical way */
 	   if (mode & AMAYA_LOAD_IMAGE)
 	     AHTRequest_setCustomAcceptHeader (me->request, IMAGE_ACCEPT_NEGOTIATION);
 	   else if (mode & AMAYA_LOAD_CSS)
 	     AHTRequest_setCustomAcceptHeader (me->request, "*/*;q=0.1,css/*");
-#ifdef ANNOTATIONS
-	   else if (content_type && content_type[0] != EOS)
-	     /* use the custom sent content_type */
-	     AHTRequest_setCustomAcceptHeader (me->request, content_type);
-#endif /* ANNOTATIONS */
 	   else
 	     AHTRequest_setCustomAcceptHeader (me->request, GENERAL_ACCEPT_NEGOTIATION);
 	   /*
 	   HTRequest_setConversion(me->request, acceptTypes, TRUE);
 	   */
 	 }
-	   HTRequest_setLanguage (me->request, acceptLanguages, TRUE);
+	 else
+	   AHTRequest_setCustomAcceptHeader (me->request, GENERAL_ACCEPT_NEGOTIATION);
+       /* language negotiation */
+       HTRequest_setLanguage (me->request, acceptLanguages, TRUE);
      }
 
    /* Common initialization for all HTML methods */
