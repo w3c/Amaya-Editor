0835c1a19e08f07a888e867a2dc4342519319d8e
pierre <pierre>
2002-07-11 09:28:45 +0000

    Fixed warnings (adding prototype)

--- thotlib/include/tree.h
+++ thotlib/include/tree.h
@@ -1064,6 +1064,32 @@ extern Element TtaSearchEmptyElement (SearchDomain scope, Element element);
 extern Element TtaSearchOtherPairedElement (Element element);
 
 /* ----------------------------------------------------------------------
+   SearchElementAmong5Types
+
+   Returns the first element typed among the 5 types provided
+   Searching can be done in a tree or starting from a given element
+   towards the beginning of the abstract tree.
+   Parameters:
+   searchedTypeX: types of element to be searched. If searchedTypeX.ElSSchema
+   is NULL, searchedTypeX must be a basic type ; then the next basic
+   element of that type will be returned, whatever its structure
+   schema.
+   scope: SearchForward or SearchInTree.
+   element: the element that is the root of the tree
+   (if scope = SearchInTree) or the starting element
+   (if scope = SearchForward).
+   Return value:
+   the element found, or NULL if no element has been found.
+   ---------------------------------------------------------------------- */
+extern Element SearchElementAmong5Types (ElementType searchedType1, 
+										 ElementType searchedType2,
+										 ElementType searchedType3,
+										 ElementType searchedType4,
+										 ElementType searchedType5, 
+										 SearchDomain scope,
+										 Element element);
+
+/* ----------------------------------------------------------------------
    TtaSearchNoPageBreak
 
    Returns the first sibling element that is not a page break.
