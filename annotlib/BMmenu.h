448b3cfbcd8ac7f81957540e72c59227f4a0915a
kahan <kahan>
2003-05-13 17:32:18 +0000

    JK: The bookmark menu definition

---
+++ annotlib/BMmenu.h
@@ -0,0 +1,38 @@
+/*
+ *
+ *  (c) COPYRIGHT INRIA and W3C, 1999-2003
+ *  Please first read the full copyright statement in file COPYRIGHT.
+ *
+ */
+
+#ifndef _BM_MENU_H__
+#define _BM_MENU_H__
+/*----------------------------------------------------------------------
+     Definitions of the menu entries used in the configuration menus
+     (MENUconf.c)
+     Jose Kahan January 2003
+  ----------------------------------------------------------------------*/
+
+#define BookmarkMenu            1
+#define mBMBookmarks            2
+#define mBMAuthor               3
+#define mBMCreated              4
+#define mBMModified             5
+#define mBMDescription          6
+#define mBMTitle                7
+#define mBMTopic                8
+#define mBMTopicTree            9
+#define MAX_BOOKMARKMENU_DLG    10
+
+#define TopicMenu               1
+#define mTMParentTopic          2
+#define mTMTopicURL             3
+#define mTMAuthor               4
+#define mTMCreated              5
+#define mTMModified             6
+#define mTMDescription          7
+#define mTMTitle                8
+#define mTMTopicTree            9
+#define MAX_TOPICMENU_DLG       10
+
+#endif /* _BM_MENU_H__ */
