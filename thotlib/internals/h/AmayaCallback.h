bc317ebd36eff10f8f47a8107f2beb670f96c0e7
gully <gully>
2004-02-17 14:50:51 +0000

    + add a AmayaContext classe used to stock contexts in callbacks
    S.GULLY

--- thotlib/internals/h/AmayaCallback.h
+++ thotlib/internals/h/AmayaCallback.h
@@ -11,6 +11,48 @@ typedef void        (*Proc) ();
 
 /*
  * =====================================================================================
+ *        Class:  AmayaContext
+ * 
+ *  Description:  this class is used to be associated with a wxObject
+ *                for exemple, a wxMenuItem could be associated with a context
+ *                and this context is used when the corresponding callback is activated
+ * 
+ *       Author:  Stephane GULLY
+ *      Created:  11/14/2003 10:49:06 AM CET
+ *     Revision:  none
+ * =====================================================================================
+ */
+class AmayaContext : public wxObjectRefData
+{
+ public:
+  AmayaContext( void * p_data = NULL) :
+    wxObjectRefData()
+    ,m_pData(p_data)
+    {
+    }
+  AmayaContext( const AmayaContext & context ) :
+    wxObjectRefData()
+    ,m_pData(context.m_pData)
+    {
+    }
+  virtual ~AmayaContext()
+    {
+    }
+  
+  void * GetData()
+    {
+      return m_pData;
+    }
+  void SetDate( void * p_data )
+    {
+      m_pData = p_data;
+    }
+ protected:
+  void * m_pData;
+};
+
+/*
+ * =====================================================================================
  *        Class:  AmayaCParam
  * 
  *  Description:  this class represent a structure for callback + parameters storage
