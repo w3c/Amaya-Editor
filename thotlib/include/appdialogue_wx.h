064b0af38180f51bcdbd92f154895724c64c7dff
gully <gully>
2004-06-15 13:53:19 +0000

    oups

---
+++ thotlib/include/appdialogue_wx.h
@@ -0,0 +1,7 @@
+#ifdef _WX
+
+/* in this file we can find thotlib function exported for amaya */
+extern void     ThotCallback ( int ref, int typedata, char *data );
+extern wxString TtaConvMessageToWX ( const char * p_message );
+
+#endif /* _WX */
