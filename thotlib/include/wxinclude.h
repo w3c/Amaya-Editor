3591344f9f43c2cf18675473d67746ad1dc42b58
gully <gully>
2004-06-17 14:38:42 +0000

    adding a Amaya => ThotLib dependency (wxAmayaTimer) S. GULLY

--- thotlib/include/wxinclude.h
+++ thotlib/include/wxinclude.h
@@ -3,4 +3,7 @@
 /* in this file we can find dialogue creation functions exported from 'amaya' */
 extern void       CreateSearchDlgWX (ThotWindow parent, char *caption);
 
+/* import wxAmayaTimer classe because it's usefull also into thotlib ... */
+#include "../../amaya/wxAmayaTimer.h"
+
 #endif /* _WX */
