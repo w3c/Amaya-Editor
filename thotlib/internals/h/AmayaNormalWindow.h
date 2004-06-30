7a0ae41fc1ba7f2db0a678ac38c70fdfd45e3da6
gully <gully>
2004-06-30 13:36:29 +0000

    uuups, I forgot 2 files.

---
+++ thotlib/internals/h/AmayaNormalWindow.h
@@ -0,0 +1,121 @@
+#ifdef _WX
+
+#ifndef __AMAYANORMALWINDOW_H__
+#define __AMAYANORMALWINDOW_H__
+
+#include "AmayaWindow.h"
+#include "windowtypes_wx.h"
+
+/*
+ * =====================================================================================
+ *        Class:  AmayaNormalWindow
+ * 
+ *  Description:  - AmayaNormalWindow is a top container
+ *                  + A normal window contains :
+ *                     - several AmayaPage
+ *                     - toolbar
+ *                     - menubar
+ *                     - statusbar
+ * 
+ * +[AmayaNormalWindow]-----------------------------------+
+ * |+----------------------------------------------------+|
+ * || MenuBar                                            ||
+ * |+----------------------------------------------------+|
+ * |+----------------------------------------------------+|
+ * || ToolBar                                            ||
+ * |+----------------------------------------------------+|
+ * |+[AmayaPanel]--+ +[AmayaNoteBook]-------------------+ |
+ * ||              | |+-----------+                     | |
+ * ||              | ||[AmayaPage]+--------------------+| |
+ * ||              | ||+------------------------------+|| |
+ * ||              | |||[AmayaFrame]                  ||| |
+ * ||              | |||                              ||| |
+ * ||              | |||                              ||| |
+ * ||              | |||  (view container)     'Top'  ||| |
+ * ||              | |||---------SplitBar-------------||| |
+ * ||              | |||[AmayaFrame]          'Bottom'||| |
+ * ||              | |||                              ||| |
+ * ||              | |||                              ||| |
+ * ||              | ||+------------------------------+|| |
+ * ||              | |+--------------------------------+| |
+ * |+--------------+ +----------------------------------+ |
+ * |+----------------------------------------------------+|
+ * || StatusBar                                          ||
+ * |+----------------------------------------------------+|
+ * +------------------------------------------------------+
+ *       Author:  Stephane GULLY
+ *      Created:  29/06/2004 04:45:34 PM CET
+ *     Revision:  none
+ * =====================================================================================
+ */
+class AmayaNormalWindow : public AmayaWindow
+{
+ public:
+  DECLARE_DYNAMIC_CLASS(AmayaNormalWindow)
+
+  AmayaNormalWindow ( int             window_id = -1
+		      ,wxWindow *     frame = NULL
+		      ,const wxPoint& pos  = wxDefaultPosition
+		      ,const wxSize&  size = wxDefaultSize );
+  virtual ~AmayaNormalWindow();
+
+  virtual AmayaPage *  GetActivePage() const;
+  virtual AmayaFrame * GetActiveFrame() const;
+
+  // --------------------------------------------- //
+  // WXAMAYAWINDOW_NORMAL interface
+  virtual AmayaPage *    CreatePage( bool attach = false, int position = 0 );
+  virtual bool           AttachPage( int position, AmayaPage * p_page );
+  virtual bool           DetachPage( int position );
+  virtual AmayaPage *    GetPage( int position ) const;
+  virtual int            GetPageCount() const;
+
+  virtual AmayaToolBar * GetAmayaToolBar();
+
+  virtual void           SetMenuBar( wxMenuBar * p_menu_bar );
+  virtual void           DesactivateMenuBar();
+  virtual void           ActivateMenuBar();
+  virtual void AppendMenu ( wxMenu * p_menu, const wxString & label );
+  virtual void AppendMenuItem ( wxMenu * 		p_menu_parent,
+				long                    id,
+				const wxString & 	label,
+				const wxString & 	help,
+				wxItemKind 		kind,
+				const AmayaCParam &     callback );
+
+  // url bar control
+  virtual wxString GetURL();
+  virtual void     SetURL ( const wxString & new_url );
+  virtual void     AppendURL ( const wxString & new_url );
+  virtual void     EmptyURLBar();
+  virtual void     SetEnableURL( bool urlenabled );
+  virtual void     SetupURLBar();
+
+ protected:
+  void OnClose( wxCloseEvent& event );
+  void OnMenuItem( wxCommandEvent& event );
+  void OnSplitterUnsplit( wxSplitterEvent& event );
+
+ protected:
+  DECLARE_EVENT_TABLE()
+  
+//  AmayaPanel *      m_aPanels[4];        // avalaible panels
+  AmayaPanel *      m_pCurrentPanel;     // current selected panel
+  AmayaNotebook *   m_pNotebook;         // tabs container
+  float             m_SlashRatio; // 0.5 => page is half splitted  
+
+  bool         m_IsFullScreenEnable;
+  bool         m_IsToolTipEnable;
+
+  wxMenuItem * m_pMenuItemToggleFullScreen;
+  wxMenuItem * m_pMenuItemToggleToolTip;
+
+  wxMenuBar *        m_pDummyMenuBar;
+  wxSplitterWindow * m_pSplitterWindow;  
+  AmayaURLBar *	     m_pURLBar;
+  AmayaToolBar *     m_pToolBar;
+};
+
+#endif // __AMAYANORMALWINDOW_H__
+
+#endif /* #ifdef _WX */
---
+++ thotlib/internals/h/AmayaSimpleWindow.h
@@ -0,0 +1,69 @@
+#ifdef _WX
+
+#ifndef __AMAYASIMPLEWINDOW_H__
+#define __AMAYASIMPLEWINDOW_H__
+
+#include "AmayaWindow.h"
+#include "windowtypes_wx.h"
+
+/*
+ * =====================================================================================
+ *        Class:  AmayaSimpleWindow
+ * 
+ *  Description:  - AmayaSimpleWindow is a top container
+ *                  + A simple window contains :
+ *                      - a AmayaFrame (= a document view)
+ *                        ex: Log view
+ *                  + Close button
+ * 
+ * +[AmayaSimpleWindow]----------------------------------+
+ * |+[AmayaFrame----------------------------------------+|
+ * ||                                                   ||
+ * ||                                                   ||
+ * ||           OpenGL canvas                           ||
+ * ||                                                   ||
+ * ||                                                   ||
+ * ||                                                   ||
+ * ||                                                   ||
+ * |+---------------------------------------------------+|
+ * |+---------------------------------------------------+|
+ * || Buttons                                           ||
+ * |+---------------------------------------------------+|
+ * +-----------------------------------------------------+
+ *       Author:  Stephane GULLY
+ *      Created:  29/06/2004 04:45:34 PM CET
+ *     Revision:  none
+ * =====================================================================================
+ */
+class AmayaSimpleWindow : public AmayaWindow
+{
+ public:
+  DECLARE_DYNAMIC_CLASS(AmayaSimpleWindow)
+
+  AmayaSimpleWindow ( int             window_id = -1
+		      ,wxWindow *     frame = NULL
+		      ,const wxPoint& pos  = wxDefaultPosition
+		      ,const wxSize&  size = wxDefaultSize );
+  virtual ~AmayaSimpleWindow();
+
+  virtual AmayaFrame * GetActiveFrame() const;
+
+  bool         AttachFrame( AmayaFrame * p_frame );
+  AmayaFrame * DetachFrame();
+
+ protected:
+  void OnClose( wxCloseEvent& event );
+  void OnCloseButton(wxCommandEvent& event);
+  void OnSplitterUnsplit( wxSplitterEvent& event );
+  void OnSize( wxSizeEvent& event );
+
+ protected:
+  DECLARE_EVENT_TABLE()
+
+  AmayaFrame *      m_pFrame;             // for simple window
+  wxBoxSizer *      m_pTopSizer;
+};
+
+#endif // __AMAYASIMPLEWINDOW_H__
+
+#endif /* #ifdef _WX */
