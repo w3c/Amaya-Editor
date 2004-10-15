8a717fe3f83e69aaba872d211ecfb47ff81bc81c
gully <gully>
2004-10-15 14:22:12 +0000

    oups S. GULLY

---
+++ thotlib/internals/h/panel.h
@@ -0,0 +1,28 @@
+/*
+ *
+ *  (c) COPYRIGHT INRIA, 1996-2004
+ *  Please first read the full copyright statement in file COPYRIGHT.
+ *
+ */
+
+#ifndef __PANELCTL_H__
+#define __PANELCTL_H__
+
+#ifndef NODISPLAY
+
+#define MAX_BUTTON  30
+
+#if defined(_WX)
+
+typedef struct _Panel_Ctl
+{
+  Proc          Call_Panel[MAX_BUTTON];     /* callbacks for XHTML panel */
+  const char *  Tooltip_Panel[MAX_BUTTON];  /* tooltips for XHTML panel */
+} Panel_Ctl;
+
+#endif /* #if defined(_WX) */
+
+
+#endif /* !NODISPLAY */
+
+#endif /* __PANELCTL_H__ */
---
+++ thotlib/internals/var/panel_tv.h
@@ -0,0 +1,24 @@
+/*
+ *
+ *  (c) COPYRIGHT INRIA, 1996-2004
+ *  Please first read the full copyright statement in file COPYRIGHT.
+ *
+ */
+
+#ifndef __PANEL_VAR__
+#define __PANEL_VAR__
+
+#ifndef NODISPLAY
+
+#ifdef _WX
+
+#include "paneltypes_wx.h"
+
+THOT_EXPORT Panel_Ctl PanelTable[WXAMAYA_PANEL_TYPE_NB+1];
+
+#endif /* _WX */
+
+#endif /* !NODISPLAY */
+  
+#endif /* #ifndef __PANEL_VAR__ */
+
