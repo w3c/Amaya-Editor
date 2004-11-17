5cf4b759dee36f42418d561018a816f2e88b0b68
gully <gully>
2004-11-17 16:54:26 +0000

    oups S. GULLY

---
+++ thotlib/include/AmayaParams.h
@@ -0,0 +1,23 @@
+#ifdef _WX
+
+#ifndef __AMAYAPARAMS_H__
+#define __AMAYAPARAMS_H__
+
+// this class is used to pass untyped multipl parameters
+class AmayaParams
+{
+ public:
+  void * param1;
+  void * param2;
+  void * param3;
+  void * param4;
+  void * param5;
+  void * param6;
+  void * param7;
+  void * param8;
+  void * param9;
+};
+
+#endif /* __AMAYASUBPANELMANAGER_H__ */
+
+#endif /* _WX */
