e56050d5d3b372d4a3618f6c812fd04ef03108da
vatton <vatton>
2005-03-08 14:37:37 +0000

    New Amaya versions. Irene

--- thotlib/internals/h/appversion.h
+++ thotlib/internals/h/appversion.h
@@ -3,10 +3,10 @@
 #ifdef _WX
 #define APP_MAJVER 9
 #define APP_MINVER 1
-#define APP_SUBVER 0
+#define APP_SUBVER 1
 #else /* _WX */
 #define APP_MAJVER 8
 #define APP_MINVER 7
-#define APP_SUBVER 2
+#define APP_SUBVER 3
 #endif /* _WX */
 #endif /* __APPVERSION_H__ */
