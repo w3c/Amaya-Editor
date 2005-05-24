633b20c096961ece350d862f54472a4bf4c20169
gully <gully>
2005-05-24 15:12:55 +0000

    new versions numbers 9.1.4 and 8.7.4 S. GULLY

--- thotlib/internals/h/appversion.h
+++ thotlib/internals/h/appversion.h
@@ -3,10 +3,10 @@
 #ifdef _WX
 #define APP_MAJVER 9
 #define APP_MINVER 1
-#define APP_SUBVER 1
+#define APP_SUBVER 4
 #else /* _WX */
 #define APP_MAJVER 8
 #define APP_MINVER 7
-#define APP_SUBVER 3
+#define APP_SUBVER 4
 #endif /* _WX */
 #endif /* __APPVERSION_H__ */
