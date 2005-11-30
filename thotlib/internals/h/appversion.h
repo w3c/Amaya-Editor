87c9a91b7f3509c87323d9245c0637678ab5efcd
vatton <vatton>
2005-11-30 13:41:20 +0000

    Prepare the new 9.3 version. with-wx is now the default option
    Irene

--- thotlib/internals/h/appversion.h
+++ thotlib/internals/h/appversion.h
@@ -2,11 +2,11 @@
 #define __APPVERSION_H__
 #ifdef _WX
 #define APP_MAJVER 9
-#define APP_MINVER 2
-#define APP_SUBVER 2
+#define APP_MINVER 3
+#define APP_SUBVER 0
 #else /* _WX */
 #define APP_MAJVER 8
 #define APP_MINVER 8
-#define APP_SUBVER 2
+#define APP_SUBVER 3
 #endif /* _WX */
 #endif /* __APPVERSION_H__ */
