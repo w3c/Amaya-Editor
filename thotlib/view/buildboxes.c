3d02019fe980f86dbd5e4369e29147076327e1a0
vatton <vatton>
2006-04-12 08:27:50 +0000

    Remove a trace. Irene

--- thotlib/view/buildboxes.c
+++ thotlib/view/buildboxes.c
@@ -3995,8 +3995,6 @@ ThotBool ComputeUpdates (PtrAbstractBox pAb, int frame, ThotBool *computeBBoxes)
                 case LtPicture:
                   if (pAb->AbChange)
                     {
-if (!strcmp (pAb->AbElement->ElLabel, "L357"))
-  printf ("AbChange L357\n");
                       /* the picture change, RAZ the structure */
                       CleanPictInfo ((ThotPictInfo *) pBox->BxPictInfo);
                       if (pAb->AbWidth.DimAbRef == NULL &&  pAb->AbWidth.DimValue == -1)
