807d303286bc93578b4de2dc2bf1bfae47b776c6
francesc <francesc>
2006-07-11 15:04:41 +0000

    Added new header file

---
+++ amaya/templates.h
@@ -0,0 +1,11 @@
+#ifndef TEMPLATE_TEMPLATES
+#define TEMPLATE_TEMPLATES
+
+#define THOT_EXPORT extern
+#include "amaya.h"
+#include "document.h"
+
+extern void CreateInstanceOfTemplate (Document doc, char *templatename, char *docname);
+
+#endif /* TEMPLATE_TEMPLATES */
+
