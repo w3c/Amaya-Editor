2f914f78b22ea97c7bda580a4625de451582de43
vatton <vatton>
2006-09-26 08:18:02 +0000

    Fix a problem with cursor move to next line. Irene

--- thotlib/editing/textcommands.c
+++ thotlib/editing/textcommands.c
@@ -1014,7 +1014,7 @@ static void MovingCommands (int code, Document doc, View view,
                 }
               if (!done)
                 {
-                  if (pBox && yDelta < pBox->BxH)
+                  if (code != 7 && pBox && yDelta < pBox->BxH)
                     yDelta = pBox->BxH; // skip almost at the bottom of the box
                   LocateLeafBox (frame, view, x, y, 0, yDelta, pBox, extendSel);
                 }
