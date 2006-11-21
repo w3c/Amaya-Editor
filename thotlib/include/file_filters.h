0841be33470508b54439dbd16c3ce52abfd11b0d
vatton <vatton>
2006-11-21 09:51:01 +0000

    Extend HTML filters Irene

--- thotlib/include/file_filters.h
+++ thotlib/include/file_filters.h
@@ -3,7 +3,7 @@
 #ifndef __FILE_FILTERS_H__
 #define __FILE_FILTERS_H__
 
-#define APPFILENAMEFILTER _T("All known files|*.html;*.htm;*.xhtml;*.xml;*.mml;*.svg;*.css;*.png;*.jpg;*.jpeg|HTML files (*.html)|*.html;*.htm;*.xhtml|XML files (*.xml)|*.xml|MathML Files (*.mml)|*.mml|SVG files (*.svg)|*.svg|CSS files (*.css)|*.css|PNG files (*.png)|*.png|JPEG files (*.jpg)|*.jpg;*.jpeg|All files (*)|*")
+#define APPFILENAMEFILTER _T("All known files|*.html;*.htm;*.xhtml;*.html.*;*.xml;*.mml;*.svg;*.css;*.png;*.jpg;*.jpeg|HTML files (*.html)|*.html;*.htm;*.xhtml;*.html.*|XML files (*.xml)|*.xml|MathML Files (*.mml)|*.mml|SVG files (*.svg)|*.svg|CSS files (*.css)|*.css|PNG files (*.png)|*.png|JPEG files (*.jpg)|*.jpg;*.jpeg|All files (*)|*")
 #define APPCSSNAMEFILTER _T("CSS files (*.css)|*.css|All files (*)|*")
 #define APPSVGNAMEFILTER _T("SVG files (*.svg)|*.svg|All files (*)|*")
 #define APPMATHNAMEFILTER _T("MathML files (*.mml)|*.mml|All files (*)|*")
