a932728daf53b8877f161052cdc63a95518ad1f7
kia <kia>
2007-08-27 13:33:30 +0000

    Change color for empty tab

--- thotlib/dialogue/AmayaNotebook.cpp
+++ thotlib/dialogue/AmayaNotebook.cpp
@@ -50,7 +50,7 @@ AmayaNotebook::AmayaNotebook(wxWindow * window , wxWindowID id)
   if (WindowBColor == -1)
     {
       // Initialize the window background colour
-    wxColour col = wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOW);
+    wxColour col = wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOWFRAME);
       WindowBColor = TtaGetThotColor (col.Red(), col.Green(), col.Blue());
     }
   SetImageList( AmayaApp::GetDocumentIconList() );
