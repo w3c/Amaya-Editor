c3fe40b041b63466328f5a3f2aabe2d64aba04f5
carcone <carcone>
2007-11-27 09:19:52 +0000

    Switched 2 includes (didn't compile on Mac OSX) Laurent

--- thotlib/dialogue/archives.c
+++ thotlib/dialogue/archives.c
@@ -1,6 +1,4 @@
 
-#include "archives.h"
-
 #include "wx/wx.h"
 #include "wx/archive.h"   // for wxArchive*
 #include "wx/zipstrm.h"   // for wxZip*Stream
@@ -8,6 +6,7 @@
 #include "wx/dir.h"       // for wxDir
 #include "wx/filename.h"  // for wxFileName
 #include "wx/string.h"    // for wxString
+#include "archives.h"
 
 #include "message_wx.h" // for TtaConvMessageToWX
 
