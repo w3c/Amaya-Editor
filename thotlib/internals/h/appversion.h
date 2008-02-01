de9391a0141db78275991421a3cbfa629946d977
vatton <vatton>
2008-02-01 07:59:13 +0000

    Irene

--- thotlib/internals/h/appversion.h
+++ thotlib/internals/h/appversion.h
@@ -1,6 +1,6 @@
 #ifndef __APPVERSION_H__
 #define __APPVERSION_H__
-#define APP_MAJVER 9
-#define APP_MINVER 99
+#define APP_MAJVER 10
+#define APP_MINVER 0
 #define APP_SUBVER 0
 #endif /* __APPVERSION_H__ */
