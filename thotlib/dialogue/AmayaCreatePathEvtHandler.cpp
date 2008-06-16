992d2568d61352d7807db3ec4bb1cfa39377e16b
vatton <vatton>
2008-06-16 15:01:37 +0000

    prepare a new handler to create curves & polylines - fred

---
+++ thotlib/dialogue/AmayaCreatePathEvtHandler.cpp
@@ -0,0 +1,192 @@
+#ifdef _WX
+
+#include "wx/wx.h"
+#include "wx/event.h"
+
+#include "thot_gui.h"
+#include "thot_sys.h"
+#include "constmedia.h"
+#include "constmedia.h"
+#include "typemedia.h"
+#include "frame.h"
+#include "libmsg.h"
+#include "message.h"
+#include "typeint.h"
+#include "undo.h"
+
+#undef THOT_EXPORT
+#define THOT_EXPORT extern
+#include "boxes_tv.h"
+#include "frame_tv.h"
+
+#include "frame_f.h"
+#include "presentationapi_f.h"
+#include "structcreation_f.h"
+#include "boxlocate_f.h"
+#include "geom_f.h"
+#include "font_f.h"
+#include "content_f.h"
+
+#ifdef _GL
+#include "glwindowdisplay.h"
+#endif /* _GL */
+
+#include "logdebug.h"
+
+
+#include "AmayaFrame.h"
+#include "AmayaCanvas.h"
+#include "AmayaCreatePathEvtHandler.h"
+
+int m_mouse_x,m_mouse_y; 
+
+IMPLEMENT_DYNAMIC_CLASS(AmayaCreatePathEvtHandler, wxEvtHandler)
+
+/*----------------------------------------------------------------------
+ *  this is where the event table is declared
+ *  the callbacks are assigned to an event type
+ *----------------------------------------------------------------------*/
+BEGIN_EVENT_TABLE(AmayaCreatePathEvtHandler, wxEvtHandler)
+EVT_CHAR( AmayaCreatePathEvtHandler::OnChar )
+
+EVT_LEFT_DOWN(	AmayaCreatePathEvtHandler::OnMouseDown) // Process a wxEVT_LEFT_DOWN event. 
+EVT_LEFT_UP(		AmayaCreatePathEvtHandler::OnMouseUp) // Process a wxEVT_LEFT_UP event. 
+EVT_LEFT_DCLICK(	AmayaCreatePathEvtHandler::OnMouseDbClick) // Process a wxEVT_LEFT_DCLICK event. 
+EVT_MIDDLE_DOWN(	AmayaCreatePathEvtHandler::OnMouseDown) // Process a wxEVT_MIDDLE_DOWN event. 
+EVT_MIDDLE_UP(	AmayaCreatePathEvtHandler::OnMouseUp) // Process a wxEVT_MIDDLE_UP event. 
+EVT_MIDDLE_DCLICK(	AmayaCreatePathEvtHandler::OnMouseDbClick) // Process a wxEVT_MIDDLE_DCLICK event. 
+EVT_RIGHT_DOWN(	AmayaCreatePathEvtHandler::OnMouseDown) // Process a wxEVT_RIGHT_DOWN event. 
+EVT_RIGHT_UP(		AmayaCreatePathEvtHandler::OnMouseUp) // Process a wxEVT_RIGHT_UP event. 
+EVT_RIGHT_DCLICK(	AmayaCreatePathEvtHandler::OnMouseDbClick) // Process a wxEVT_RIGHT_DCLICK event. 
+EVT_MOTION(		AmayaCreatePathEvtHandler::OnMouseMove) // Process a wxEVT_MOTION event. 
+EVT_MOUSEWHEEL(	AmayaCreatePathEvtHandler::OnMouseWheel) // Process a wxEVT_MOUSEWHEEL event. 
+END_EVENT_TABLE()
+
+/*----------------------------------------------------------------------
+ *----------------------------------------------------------------------*/
+AmayaCreatePathEvtHandler::AmayaCreatePathEvtHandler() : wxEvtHandler()
+{
+}
+
+/*----------------------------------------------------------------------
+ *----------------------------------------------------------------------*/
+AmayaCreatePathEvtHandler::AmayaCreatePathEvtHandler(AmayaFrame * p_frame, int x_min, int y_min, int x_max, int y_max, int *nb_points, Document doc, int shape_number)
+  : wxEvtHandler()
+  ,m_IsFinish(false)
+  ,m_pFrame(p_frame)
+  ,m_FrameId(p_frame->GetFrameId())
+  ,m_xmin(x_min)
+  ,m_ymin(y_min)
+  ,m_xmax(x_max)
+  ,m_ymax(y_max)
+  ,m_ShapeNumber(shape_number)
+  ,m_NbPoints(nb_points)
+  ,m_document(doc)
+    
+{
+
+  *m_NbPoints = 0;
+  if (m_pFrame)
+    {
+      /* attach this handler to the canvas */
+      AmayaCanvas * p_canvas = m_pFrame->GetCanvas();
+      p_canvas->PushEventHandler(this);
+
+      /* assign a cross mouse cursor */
+      m_pFrame->GetCanvas()->SetCursor( wxCursor(wxCURSOR_CROSS) );
+      m_pFrame->GetCanvas()->CaptureMouse();
+    }
+}
+
+/*----------------------------------------------------------------------
+ *----------------------------------------------------------------------*/
+AmayaCreatePathEvtHandler::~AmayaCreatePathEvtHandler()
+{
+
+  if (m_pFrame)
+    {
+      /* detach this handler from the canvas (restore default behaviour) */
+      AmayaCanvas * p_canvas = m_pFrame->GetCanvas();
+      p_canvas->PopEventHandler(false /* do not delete myself */);
+      
+      /* restore the default cursor */
+      m_pFrame->GetCanvas()->SetCursor( wxNullCursor );
+
+      m_pFrame->GetCanvas()->ReleaseMouse();
+    }
+}
+
+/*----------------------------------------------------------------------
+ *----------------------------------------------------------------------*/
+bool AmayaCreatePathEvtHandler::IsFinish()
+{
+  return m_IsFinish;
+}
+
+/*----------------------------------------------------------------------
+ *----------------------------------------------------------------------*/
+void AmayaCreatePathEvtHandler::OnChar( wxKeyEvent& event )
+{
+  m_IsFinish = true;
+}
+
+/*----------------------------------------------------------------------
+ *       Class:  AmayaCreatePathEvtHandler
+ *      Method:  OnMouseDown
+ * Description:  handle mouse button down events
+ -----------------------------------------------------------------------*/
+void AmayaCreatePathEvtHandler::OnMouseDown( wxMouseEvent& event )
+{
+}
+
+/*----------------------------------------------------------------------
+ *       Class:  AmayaCreatePathEvtHandler
+ *      Method:  OnMouseUp
+ * Description:  handle mouse button up events
+ -----------------------------------------------------------------------*/
+void AmayaCreatePathEvtHandler::OnMouseUp( wxMouseEvent& event )
+{
+  if (IsFinish())
+    return;
+
+  (*m_NbPoints)++;
+
+
+}
+
+/*----------------------------------------------------------------------
+ *       Class:  AmayaCreatePathEvtHandler
+ *      Method:  OnMouseDbClick
+ * Description:  handle mouse dbclick events
+ -----------------------------------------------------------------------*/
+void AmayaCreatePathEvtHandler::OnMouseDbClick( wxMouseEvent& event )
+{
+  m_IsFinish = true;
+}
+
+/*----------------------------------------------------------------------
+ *       Class:  AmayaCreatePathEvtHandler
+ *      Method:  OnMouseMove
+ * Description:  handle mouse move events
+ -----------------------------------------------------------------------*/
+void AmayaCreatePathEvtHandler::OnMouseMove( wxMouseEvent& event )
+{
+  m_mouse_x = event.GetX();
+  m_mouse_y = event.GetY();
+
+  MouseCoordinatesToSVG(m_document, m_pFrame, m_xmin, m_xmax, m_ymin, m_ymax,
+			FALSE, &m_mouse_x, &m_mouse_y);
+
+  m_pFrame->GetCanvas()->Refresh();
+}
+
+/*----------------------------------------------------------------------
+ *       Class:  AmayaCreatePathEvtHandler
+ *      Method:  OnMouseWheel
+ * Description:  handle mouse wheel events
+ -----------------------------------------------------------------------*/
+void AmayaCreatePathEvtHandler::OnMouseWheel( wxMouseEvent& event )
+{
+}
+
+#endif /* _WX */
---
+++ thotlib/internals/h/AmayaCreatePathEvtHandler.h
@@ -0,0 +1,52 @@
+#ifdef _WX
+
+#ifndef __AMAYACREATEPATHEVTHANDLER_H__
+#define __AMAYACREATEPATHEVTHANDLER_H__
+
+#include "wx/wx.h"
+#include "wx/event.h"
+
+#include "thot_gui.h"
+#include "thot_sys.h"
+#include "constmedia.h"
+#include "typeint.h"
+
+class AmayaFrame;
+
+class AmayaCreatePathEvtHandler : public wxEvtHandler
+{
+ public:
+  DECLARE_DYNAMIC_CLASS(AmayaCreatePathEvtHandler)
+
+  AmayaCreatePathEvtHandler();
+  AmayaCreatePathEvtHandler(AmayaFrame * p_frame, int x_min, int y_min, int x_max, int y_max, int *nb_points, Document doc, int shape_number);
+
+  virtual ~AmayaCreatePathEvtHandler();
+
+  bool IsFinish();
+
+ protected:
+  DECLARE_EVENT_TABLE()
+  void OnChar( wxKeyEvent& event );
+  void OnMouseMove( wxMouseEvent& event );
+  void OnMouseWheel( wxMouseEvent& event );
+  void OnMouseDbClick( wxMouseEvent& event );
+  void OnMouseUp( wxMouseEvent& event );
+  void OnMouseDown( wxMouseEvent& event );
+
+ protected:
+  bool m_IsFinish;
+
+  AmayaFrame * m_pFrame;
+  int m_FrameId;
+  int m_xmin, m_ymin, m_xmax, m_ymax;
+
+  int m_ShapeNumber;
+  int *m_NbPoints;
+  int m_document;
+
+};
+
+#endif /* __AMAYACREATEPATHEVTHANDLER_H__ */
+
+#endif /* #ifdef _WX */
