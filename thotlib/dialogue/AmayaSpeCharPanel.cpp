33510b47d7d8cb2b3b78ef8ca364fc00340905e3
vatton <vatton>
2009-09-16 11:14:37 +0000

    Add new characters Irene

--- thotlib/dialogue/AmayaSpeCharPanel.cpp
+++ thotlib/dialogue/AmayaSpeCharPanel.cpp
@@ -605,14 +605,25 @@ AmayaSpeChar filtre_divers[] =
     {0x201D, "right double quotation mark"},
     {0x00AB, "left guillemet"}, /* << */
     {0x00BB, "right guillemet"}, /* >> */
+
     {0x00A1, "inverted exclamation mark"},
     {0x2030, "per mille sign"},
     {0x00B0, "degree sign"},
-    {0x00B6, "paragraph sign"},
+    {0x2026, "horizontal ellipsis"},
+    {0x2013, "en dash"},
+    {0x2014, "em dash"},
+    {0x00F7, "division sign"},
+    {0x00A6, "broken bar"},
+
     {0x0153, "oe"},
     {0x0152, "OE"},
     {0x00E6, "ae"},
     {0x00C6, "AE"},
+    {0x00FE, "thorn"},
+    {0x00DE, "THORN"},
+    {0x00DF, "eszett"},
+    {0x017F, "long s"},
+
     {0x20AC, "euro"},
     {0x00A2, "cent"},
     {0x00A3, "pound"},
@@ -621,14 +632,16 @@ AmayaSpeChar filtre_divers[] =
     {0x00A4, "currency"},
     {0x2020, "dagger"},
     {0x2021, "double dagger"},
-    {0x2013, "en dash"},
-    {0x2014, "em dash"},
-    {0x00A6, "broken bar"},
+
+    {0x00B6, "paragraph sign"},
     {0x00A7, "section"},
     {0x00A9, "copyright"},
     {0x00AE, "registered"},
     {0x2122, "trade mark"},
     {0x2234, "therefore"},
+    {0x00D8, "O slash"},
+    {0x2720, "maltese cross"},
+
     {0x2605, "black star"},
     {0x2606, "white star"},
     {0x2663, "black club suit"},
@@ -637,6 +650,7 @@ AmayaSpeChar filtre_divers[] =
     {0x2660, "black spade suit"},
     {0x25A0, "black square"},
     {0x25A1, "white square"},
+
     {0x25B2, "black up-pointing triangle"},
     {0x25B3, "white up-pointing triangle"},
     {0x25BC, "black down-pointing triangle"},
@@ -645,14 +659,13 @@ AmayaSpeChar filtre_divers[] =
     {0x25C7, "white diamond"},
     {0x25CB, "white circle"},
     {0x25CF, "black circle"},
+
     {0x25D0, "circle with left half"},
     {0x25D1, "circle with right half"},
     {0x25D2, "circle with lower half"},
     {0x25D3, "circle with upper half"},
-    {0x00D8, "O slash"},
     {0x25ca, "lozenge"},
     {0x25B1, "white parallelogram"},
-    {0x2720, "maltese cross"},
     {0x260E, "black telephone"},
     {0x2460, "circled 1"},
     {-1, ""}
