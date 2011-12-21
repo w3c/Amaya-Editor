b2f4a2baca3239b560dd8d327f66bcaf258c28fe
vatton <vatton>
2011-12-21 10:29:02 +0000

    update version number

--- thotlib/internals/h/appversion.h
+++ thotlib/internals/h/appversion.h
@@ -1,6 +1,6 @@
 #ifndef __APPVERSION_H__
 #define __APPVERSION_H__
 #define APP_MAJVER 11
-#define APP_MINVER 3
-#define APP_SUBVER 1
+#define APP_MINVER 4
+#define APP_SUBVER 4
 #endif /* __APPVERSION_H__ */
